6d2c8f8870653fbf1358a81545d4ed8123b53f14
outstandyy <zinchenko.vladislav.1995@gmail.com>
2022-11-09 22:00:09 +0100

    Change Hello World to Hello Cloud

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const message = 'Hello Cloud\n';
+
 app.get('/', (req, res) => {
-	res.send('Hello world!\n');
+	res.send(message);
 });
 
 app.listen(8080, () => console.log('This app listening on port 8080'));
