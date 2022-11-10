d57b2201b1db93f7ac53b020d674663fe93cf9e4
outstandyy <zinchenko.vladislav.1995@gmail.com>
2022-11-10 13:36:52 +0100

    update

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const { hostname } = require('os');
 
 const message = 'Hello Cloud\n';
+const port = 8080;
 
 app.get('/', (req, res) => {
 	res.send(message);
 });
 
-app.listen(8080, () => console.log(`Server running at http://${hostname()}:${port}/`));
+app.listen(port, () => console.log(`Server running at http://${hostname()}:${port}/`));
