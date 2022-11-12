39b1451f094d63795e4000192ac82eb526c00b20
outstandyy <zinchenko.vladislav.1995@gmail.com>
2022-11-12 17:33:04 +0100

    Add stack name to server output

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const { hostname } = require('os');
 
+const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
 const port = 8080;
-const message = `${message}; host: ${hostname()}:${port}\n`
+const message = `Hello from host: ${hostname()}:${port} in ${STACK_NAME}\n`
 
 app.get('/', (req, res) => {
 	res.send(message);
